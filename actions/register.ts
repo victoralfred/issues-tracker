f069030f890a2abeffd7672cad49c7eb5da89e83
Victor Oseghale <victoralfred32@gmail.com>
2024-09-01 23:39:01 +0200

    Configured Prisma account and User model

--- actions/register.ts
+++ actions/register.ts
@@ -1,12 +1,15 @@
 "use server"
+import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
 import z from 'zod';
 
 // Useing server action
 export const register = async (values: z.infer<typeof RegisterSchema>)=>{
+
     const validated = RegisterSchema.safeParse(values)
     if(!validated.success){
         return {error: 'Invalid fields'}
     }
+
     return {success: "Email Send!"}
 }
