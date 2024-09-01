98bc66a46e42ee6803bfceb63a812137ed4b6880
Victor Oseghale <victoralfred32@gmail.com>
2024-09-02 01:14:08 +0200

    Configured user model for interracting with database

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,8 @@
 import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
 import z from 'zod';
+import bcrypt from 'bcrypt'
+import { getUserByEmail } from '@/data/user';
 
 // Useing server action
 export const register = async (values: z.infer<typeof RegisterSchema>)=>{
@@ -9,7 +11,21 @@ export const register = async (values: z.infer<typeof RegisterSchema>)=>{
     const validated = RegisterSchema.safeParse(values)
     if(!validated.success){
         return {error: 'Invalid fields'}
-    }
-
-    return {success: "Email Send!"}
+    };
+    const {name, email, password} = validated.data;
+    // Check if user already exist
+    const userExist = await getUserByEmail(email);
+    if(userExist){
+        return {error: "Email is already taken"}
+    };
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await db.user.create({
+        data:{
+            name,
+             email, 
+             password : hashedPassword
+        }
+    })
+    // TODO: Send verification token email
+    return {success: "User successfully registered"}
 }
