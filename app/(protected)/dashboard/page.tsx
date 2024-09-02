c72b8561069175121e47c675ee8b2daae43de990
Victor Oseghale <victoralfred32@gmail.com>
2024-09-02 22:56:56 +0200

    Tested the signout

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import {auth} from '@/auth';
+import {auth, signOut} from '@/auth';
 
 const Dashboard = async() => {
     // Get an instance of auth, and if the user is loged out, they should not be able to access dashboard
     const session = await auth();
   return (
-    <div>{JSON.stringify(session)}</div>
+    <div>
+      <p className='text-sm'>{JSON.stringify(session)}</p>
+    <form action={async()=>{
+      "use server"
+      await signOut();
+    }}>
+
+    <button type='submit'>
+      Sign out
+    </button>
+    </form>
+    </div>
   )
 }
 
