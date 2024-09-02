c832088f0de8500cf4817e919347ce57531745c7
Victor Oseghale <victoralfred32@gmail.com>
2024-09-02 23:33:42 +0200

    Roles config: Configured user roles and added Roles to session

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import {PrismaAdapter} from '@auth/prisma-adapter'
 import { db } from "@/lib/db"
 import authConfig from "@/auth.config"
+import { getUserByID } from "./data/user"
  
 export const { 
   handlers:{GET, POST}, signIn, signOut, auth 
@@ -9,10 +10,15 @@ export const {
   callbacks:{
     // What we want to do is add the user id to the sesssion token to make it available in all component using session
     async session({token, session}){
-      if(token.sub && session.user) session.user.id = token.sub
+      if(token.sub && session.user) session.user.id = token.sub;
+      if(token.role && session.user) session.user.role = token.role;
       return session;
     },
     async jwt({token}) {
+      if(!token.sub) return token
+      const currentUser = await getUserByID(token.sub);
+      if(!currentUser) return token;
+      token.role = currentUser.userRoles;
       return token;
     }
   },
--- data/user.ts
+++ data/user.ts
@@ -16,3 +16,4 @@ export const getUserByID = async(id: string)=>{
         return null
     }
 };
+
