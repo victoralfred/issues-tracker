b318d8da291a33d548061a468fd55fcad9a08e10
Victor Oseghale <victoralfred32@gmail.com>
2024-09-03 21:46:16 +0200

    Added email verification generation

--- data/verification-token.ts
+++ data/verification-token.ts
@@ -1,15 +1,24 @@
 import { db } from "@/lib/db";
-
-export const getVerificationTokenByEmail = async(email_address: string)=>{
+/**
+ * Resource used for interracting with the token table to fetch token by a given user email address
+ * @param email_address 
+ * @returns VerificationToken
+ */
+export const getVerificationTokenByEmail = async(email: string)=>{
     try{
        const verificatioN_token = await db.verifiationToken.findFirst({
-            where: {email: email_address}
+            where: {email: email}
         });
         return verificatioN_token;
     }catch(error){
         return null;
     }
 }
+/**
+ * A l
+ * @param token 
+ * @returns 
+ */
 export const getVerificationByTokenByToken = async(token: string)=>{
     try{
        const verificatioN_token = await db.verifiationToken.findUnique({
