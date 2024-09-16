661d1582e5dc5395427865fa063c344b371d1f6c
Victor Oseghale <victoralfred32@gmail.com>
2024-09-16 11:22:00 +0200

    Rëmove local test

--- app/api/encryption/route.tsx
+++ app/api/encryption/route.tsx
@@ -7,20 +7,21 @@ interface response {
     dateTime: string
 }
 export const GET= async(request: NextRequest)=>{
-    try{
-       const response = await axios.get("http://172.19.64.1:7990/rest/api/v1/encryption",{
-        timeout:20
-       })
-        const {pk, pkvHash,dateTime}: response = response.data
-        const publicKey = `-----BEGIN PUBLIC KEY-----\n${pk}\n-----END PUBLIC KEY-----`;
-        const encrypt = new JSEncrypt();
-        encrypt.setPublicKey(publicKey);
-        const dataToEncrypt = 'James wella';
-        const encrypted = encrypt.encrypt(dataToEncrypt);
-        return NextResponse.json({encrypted});
+    // try{
+    //    const response = await axios.get("http://172.19.64.1:7990/rest/api/v1/encryption",{
+    //     timeout:20
+    //    })
+    //     const {pk, pkvHash,dateTime}: response = response.data
+    //     const publicKey = `-----BEGIN PUBLIC KEY-----\n${pk}\n-----END PUBLIC KEY-----`;
+    //     const encrypt = new JSEncrypt();
+    //     encrypt.setPublicKey(publicKey);
+    //     const dataToEncrypt = 'James wella';
+    //     const encrypted = encrypt.encrypt(dataToEncrypt);
+    //     return NextResponse.json({encrypted});
   
-    }catch(error){
-        console.log(error)
-    }
+    // }catch(error){
+    //     console.log(error)
+    // }
+    return NextResponse.json({message:"Hello World"});
    
 }
